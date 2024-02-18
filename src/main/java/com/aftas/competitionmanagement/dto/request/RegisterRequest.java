94b34ac7306624921887fcc011037f1b66339c93
ErrayAbdellah <mahdiabdllah121@gmail.com>
2024-02-19 00:13:02 +0100

    AuthenticationRequest RegisterRequest is added

---
+++ src/main/java/com/aftas/competitionmanagement/dto/request/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.spring.s3cur1ty.dto.request;
+
+import com.spring.s3cur1ty.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+//    private int role;
+}
